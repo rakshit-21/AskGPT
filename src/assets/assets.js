4a9f1f4e27f37b3c9b0910b565a96f6d61e7d442
rakshit suneja <rakshits2105@gmail.com>
2025-05-23 15:29:55 +0530

    Sidebar

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -9,6 +9,9 @@ import message_icon from './message_icon.jpg';
 import plus_icon from './plus_icon.jpg';
 import send_icon from './send_icon.jpg';
 import askgpt_icon from './askgpt_icon.png';
+import settings_icon from './settings_icon.png';
+import help_icon from './help_icon.png';
+
 
 export const assets = {
     bulb_icon,
@@ -20,5 +23,7 @@ export const assets = {
     message_icon,
     plus_icon,
     send_icon,
-    askgpt_icon
+    askgpt_icon,
+    settings_icon,
+    help_icon
 };
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,42 @@
     import React from 'react'
+    import { useState } from 'react'
     import './Sidebar.css'
     import { assets } from '../../assets/assets'
 
     const Sidebar = () => {
+
+        const[extended,setExtented]=useState(false)
     return (
-        <div className='Sidebar'>
+        <div className='sidebar'>
             <div className='top'>
                 <img className='menu' src={assets.menu_icon} alt="" />
-                <div className='newchat'>
+                <div className='new-chat'>
                     <img className='plus' src={assets.plus_icon} alt="" />
-                    <p>New Chat</p>
-
+                    {extended?<p>New Chat</p>:null}
+                </div>
+                {extended?
+                <div className='recent'>
+                    <p className='recent-title'>Recent</p>
+                
+                <div className='recent-entry'>
+                    <img src={assets.message_icon} alt=''/>
+                    <p>What is react...</p>
+                </div>
+                </div>
+                :null}
+            </div>
+            <div className='bottom'>
+                <div className='bottom-item'>
+                    <img src={assets.history_icon} alt=''/>
+                    {extended?<p>Chat History</p>:null}
+                </div>
+                <div className='bottom-item'>
+                    <img src={assets.settings_icon} alt=''/>
+                    {extended?<p>Settings</p>:null}
+                </div>
+                <div className='bottom-item'>
+                    <img src={assets.help_icon} alt=''/>
+                    {extended?<p>Help</p>:null}
                 </div>
             </div>
         </div>
